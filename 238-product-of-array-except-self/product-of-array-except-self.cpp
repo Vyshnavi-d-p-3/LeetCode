8a4d72150aadecd68b4dbe0d9df46549f81d69f5
Vyshnavi-d-p-3 <vyshnavi.dyvandinnepullareddy@gmail.com>
2025-06-09 07:23:48 -0700

    Time: 4 ms (22.35%) | Memory: 40.1 MB (79.59%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.cpp
+++ 238-product-of-array-except-self/product-of-array-except-self.cpp
@@ -15,22 +15,36 @@ public:
     //    }
     //    return result;
 
-    // Approach 2 - where we will maintain two arrays preffix and suffix and calculate the product
+    // Approach 2 - where we will maintain two arrays (left_product - prefix prod) and right_product(suffix prod) and calculate the product - Time Complexity O(N) and Space Complecity O(N)[left_product and right_product space]
+    // int n = nums.size();
+    // vector<int> output(n);
+    // vector<int> left_product(n);
+    // vector<int> right_product(n);
+    // left_product[0] = 1;
+    // right_product[n-1] = 1;
+    // for(int i = 1; i < n ; i++)
+    // {
+    //     left_product[i] = left_product[i-1] * nums[i-1];
+    // }
+    // for(int i = n-2; i >= 0; i--){
+    //     right_product[i] = right_product[i+1] * nums[i+1];
+    // }
+    // for(int i=0; i<nums.size() ;i++){
+    //     output[i] = left_product[i]*right_product[i];
+    // }
+    // return output;
+
+    // Approach 3 - Similar approach as above without creating space for left and righ product - Time Compelxity is O(N) and Space Complexity is O(1)
     int n = nums.size();
     vector<int> output(n);
-    vector<int> left_product(n);
-    vector<int> right_product(n);
-    left_product[0] = 1;
-    right_product[n-1] = 1;
-    for(int i = 1; i < n ; i++)
-    {
-        left_product[i] = left_product[i-1] * nums[i-1];
+    output[0] = 1;
+    for(int i = 1 ; i<n ; i++){
+        output[i] =  output[i-1] * nums[i-1];
     }
-    for(int i = n-2; i >= 0; i--){
-        right_product[i] = right_product[i+1] * nums[i+1];
-    }
-    for(int i=0; i<nums.size() ;i++){
-        output[i] = left_product[i]*right_product[i];
+    int right = 1;
+    for(int i = n-1; i>=0; i--){
+        output[i] *= right;
+        right *= nums[i];
     }
     return output;
     }
