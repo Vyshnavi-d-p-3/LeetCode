d8159b9ad121040ecb2d7dfedb66a38259e1663b
Vyshnavi-d-p-3 <vyshnavi.dyvandinnepullareddy@gmail.com>
2025-06-15 18:04:30 -0700

    Time: 84 ms (53.02%) | Memory: 89 MB (29.30%) - LeetSync

--- 128-longest-consecutive-sequence/longest-consecutive-sequence.cpp
+++ 128-longest-consecutive-sequence/longest-consecutive-sequence.cpp
@@ -1,24 +1,38 @@
 class Solution {
 public:
     int longestConsecutive(vector<int>& nums) {
-        if(nums.size() == 0){
-            return 0;
-        }
-        sort(nums.begin(), nums.end());
-        int count = 1;
-        int temp = 1 ;
-        
-        for(int i = 0; i < nums.size()-1; i++) {
-            if((nums[i] == nums[i+1]) || (nums[i] == (nums[i+1] - 1) )) {
-                if((nums[i] == (nums[i+1] - 1))){
-                    temp++; 
-                }            
-            }else {
-                count = max(temp, count);
-                temp = 1;
+        // Approach 1 : Time Complexity O(nlogn) SpaceComplexity O(K)
+        // if(nums.size() == 0){
+        //     return 0;
+        // }
+        // sort(nums.begin(), nums.end());
+        // int count = 1;
+        // int temp = 1 ;
+        // for(int i = 0; i < nums.size()-1; i++) {
+        //     if((nums[i] == nums[i+1]) || (nums[i] == (nums[i+1] - 1) )) {
+        //         if((nums[i] == (nums[i+1] - 1))){
+        //             temp++; 
+        //         }            
+        //     }else {
+        //         count = max(temp, count);
+        //         temp = 1;
+        //     }
+        //     count = max(temp, count);
+        // }
+        // return count;
+
+        // Approach 2 : Using unordered_set which has Time Complexity - O(N) and Space Complexity - O(N)
+        unordered_set<int> numSet(nums.begin(), nums.end());
+        int longest = 0;
+        for(int num:numSet){
+            if(numSet.find(num - 1) == numSet.end()){
+                int length = 1;
+                while(numSet.find(num+length) != numSet.end()){
+                    length++;
+                }
+                longest = max(longest, length);
             }
-            count = max(temp, count);
         }
-        return count;
+        return longest;
     }
 };
