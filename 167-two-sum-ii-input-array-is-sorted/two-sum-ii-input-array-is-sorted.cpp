224411b641c00c5e7190ae3527ae1f1312e94ce9
Vyshnavi-d-p-3 <vyshnavi.dyvandinnepullareddy@gmail.com>
2025-06-21 04:22:25 -0700

    Time: 0 ms (100.00%) | Memory: 19.3 MB (93.44%) - LeetSync

---
+++ 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& numbers, int target) {
+    // Approach 1 : Brute Force Time Complexity is O(N*2)
+    //     int n = numbers.size();
+    //     vector<int> result;
+    //     for(int i=0; i < n; i++){
+    //         int ind1 = i;
+    //         for(int j = i+1 ; j < n; j++){
+    //             if(numbers[i] + numbers[j] == target){
+    //                 result.push_back(i+1);
+    //                 result.push_back(j+1);
+    //                 break;
+    //             }
+    //         }
+    //     }
+    // return result;
+
+    // Approach 2 : Two Pointer approach - Time Complexity is O(N)
+        int n = numbers.size();
+        int left  = 0;
+        int right = n-1;
+        vector<int> result;
+        int sum ;
+        while(left < right){
+            sum = numbers[left] + numbers[right];
+            if(sum == target) {
+               return {left+1 , right+1}; // 1-based Indexing
+            }
+            else if(sum < target) left++;
+            else{
+                right--;
+            }
+        }
+        return {};
+    }
+};
