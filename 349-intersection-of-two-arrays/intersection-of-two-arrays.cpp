f7ede344da1a17d27877cee41c3b6e6e74520e96
Vyshnavi-d-p-3 <vyshnavi.dyvandinnepullareddy@gmail.com>
2025-07-23 02:37:55 -0700

    Time: 2 ms (44.18%) | Memory: 14.7 MB (16.66%) - LeetSync

--- 349-intersection-of-two-arrays/intersection-of-two-arrays.cpp
+++ 349-intersection-of-two-arrays/intersection-of-two-arrays.cpp
@@ -1,14 +1,24 @@
 class Solution {
 public:
     vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
-        unordered_set<int> set1(nums1.begin(), nums1.end());
-        unordered_set<int> set2(nums2.begin(), nums2.end());
-        vector<int> result;
-        for(const auto& num : set1) {
-            if(set2.find(num) != set2.end()) {
-                result.push_back(num);
+        // Approach 1 : Using Unordered Set - TC : O(N), SC : O(N+M)
+        // unordered_set<int> set1(nums1.begin(), nums1.end());
+        // unordered_set<int> set2(nums2.begin(), nums2.end());
+        // vector<int> result;
+        // for(const auto& num : set1) {
+        //     if(set2.find(num) != set2.end()) {
+        //         result.push_back(num);
+        //     }
+        // }
+        // return result;
+        
+        unordered_set<int> hashset(nums1.begin(), nums1.end());
+        unordered_set<int> result;
+        for(int num : nums2) {
+            if(hashset.count(num)) {
+                result.insert(num);
             }
         }
-        return result;
+        return vector<int>(result.begin(), result.end());
     }
 };
