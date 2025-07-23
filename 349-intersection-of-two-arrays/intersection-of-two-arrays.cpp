e64f1d7c10d515e55ec4508270c5e9cb20e1f016
Vyshnavi-d-p-3 <vyshnavi.dyvandinnepullareddy@gmail.com>
2025-07-23 02:48:58 -0700

    Time: 9 ms (5.41%) | Memory: 14.6 MB (28.78%) - LeetSync

--- 349-intersection-of-two-arrays/intersection-of-two-arrays.cpp
+++ 349-intersection-of-two-arrays/intersection-of-two-arrays.cpp
@@ -1,6 +1,23 @@
 class Solution {
 public:
     vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
+        // Approach 1 : Brute Force  - TC : O(n*m), SC : O(n+m)
+        vector<int> res;
+        vector<int> flag(nums2.size(), 0);
+        set<int> inserted;
+        for(int i = 0; i < nums1.size(); i++) {
+            for(int j = 0; j < nums2.size(); j++) {
+                if(nums1[i] == nums2[j] && flag[j] == 0 && inserted.count(nums1[i]) == 0) {
+                    res.push_back(nums1[i]);
+                    flag[j] = 1;
+                    inserted.insert(nums1[i]);
+                }
+            }
+           
+        }
+ return res;
+
+
         // Approach 1 : Using Unordered Set - TC : O(N), SC : O(N+M)
         // unordered_set<int> set1(nums1.begin(), nums1.end());
         // unordered_set<int> set2(nums2.begin(), nums2.end());
@@ -12,13 +29,14 @@ public:
         // }
         // return result;
         
-        unordered_set<int> hashset(nums1.begin(), nums1.end());
-        unordered_set<int> result;
-        for(int num : nums2) {
-            if(hashset.count(num)) {
-                result.insert(num);
-            }
-        }
-        return vector<int>(result.begin(), result.end());
+        // Approach 2 : Using Set - TC : O(N+M), SC : O(N)
+        // unordered_set<int> hashset(nums1.begin(), nums1.end());
+        // unordered_set<int> result;
+        // for(int num : nums2) {
+        //     if(hashset.count(num)) {
+        //         result.insert(num);
+        //     }
+        // }
+        // return vector<int>(result.begin(), result.end());
     }
 };
