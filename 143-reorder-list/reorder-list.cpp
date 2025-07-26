af1c010cfbe0c144174f6b40b2b5d57588085ad5
Vyshnavi-d-p-3 <vyshnavi.dyvandinnepullareddy@gmail.com>
2025-07-26 16:16:35 +0530

    Time: 0 ms (100.00%) | Memory: 22.8 MB (56.79%) - LeetSync

--- 143-reorder-list/reorder-list.cpp
+++ 143-reorder-list/reorder-list.cpp
@@ -11,35 +11,72 @@
 class Solution {
 public:
     void reorderList(ListNode* head) {
-        stack<ListNode*> s;
-        ListNode* curr = head;
-        while(curr){
-            s.push(curr);
-            curr=curr->next;
+        // Approach 1 : Using Stack, TC : O(N), SC : O(N)
+        // stack<ListNode*> s;
+        // ListNode* curr = head;
+        // while(curr){
+        //     s.push(curr);
+        //     curr=curr->next;
+        // }
+
+        // curr = head;
+        // unordered_map<ListNode*, bool> vis;
+
+        // while(true) {
+        //     ListNode* last = s.top();
+        //     s.pop();
+        //     ListNode* next = curr->next;
+        //     vis[curr] = true;
+        //     if(vis[last]){
+        //         curr->next = NULL;
+        //         break;
+        //     }
+        //     curr->next = last;
+        //     vis[last] = true;
+        //     curr = curr->next;
+        //     if(vis[next]) {
+        //         curr->next = NULL;
+        //         break;
+        //     }
+        //     curr->next = next;
+        //     curr = curr->next;
+        // }
+
+        // Approach 2 :
+
+        // base case : LinkedList is empty
+        if(!head) return;
+
+        // Finding the middle with the help of two pointer approach
+        ListNode *tmp = head, *half = head, *prev = NULL;
+        while(tmp->next && tmp->next->next) {
+            tmp = tmp->next->next;
+            half = half->next;
         }
 
-        curr = head;
-        unordered_map<ListNode*, bool> vis;
+        // Adding one bit in case of lists with even length
+        if(tmp->next) half = half->next;
 
-        while(true) {
-            ListNode* last = s.top();
-            s.pop();
-            ListNode* next = curr->next;
-            vis[curr] = true;
-            if(vis[last]){
-                curr->next = NULL;
-                break;
-            }
-            curr->next = last;
-            vis[last] = true;
-            curr = curr->next;
-            if(vis[next]) {
-                curr->next = NULL;
-                break;
-            }
-            curr->next = next;
-            curr = curr->next;
+        // Now reverse the second half
+        while(half) {
+            tmp = half->next;
+            half->next = prev;
+            prev = half;
+            half = tmp;
+        }
+        half = prev;
+
+        // After reversing the secind half, let's merge both the halfes
+        while(head && half) {
+            tmp = head->next;
+            prev = half->next;
+            head->next = half;
+            half->next = tmp;
+            head = tmp;
+            half = prev;
         }
 
+        // Base case : closing when we had even length arrays
+        if(head && head->next) head->next->next = NULL;
     }
 };
