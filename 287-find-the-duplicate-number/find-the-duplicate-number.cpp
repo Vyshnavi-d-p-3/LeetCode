274db7b3ad54ac13aedda4c627e2ba92270264b4
Vyshnavi-d-p-3 <vyshnavi.dyvandinnepullareddy@gmail.com>
2025-07-31 09:56:20 +0530

    Time: 0 ms (100.00%) | Memory: 65.1 MB (42.62%) - LeetSync

--- 287-find-the-duplicate-number/find-the-duplicate-number.cpp
+++ 287-find-the-duplicate-number/find-the-duplicate-number.cpp
@@ -1,13 +1,35 @@
 class Solution {
 public:
     int findDuplicate(vector<int>& nums) {
-        unordered_set<int> s;
-        for(int num : nums) {
-            if(s.count(num)) {
-                return num;
+        // Appraoch 1 - Set TC : O(n*2*logn), SC : O(N);
+        // unordered_set<int> s;
+        // for(int num : nums) {
+        //     if(s.count(num)) {
+        //         return num;
+        //     }
+        //     s.insert(num);
+        // }
+        // return -1;
+
+        // Approach 2 : Floyds Algorithm - Hair and Tail Algorithm , TC : O(n), SC : O(1) 
+        int slow = nums[0];
+        int fast = nums[0];
+
+        while(true) {
+            slow = nums[slow];
+            fast = nums[nums[fast]];
+
+            if(slow == fast) {
+                break;
             }
-            s.insert(num);
         }
-        return -1;
+
+        int slow2 = nums[0];
+
+        while(slow != slow2) {
+            slow = nums[slow];
+            slow2 = nums[slow2];
+        }
+        return slow;
     }
 };
