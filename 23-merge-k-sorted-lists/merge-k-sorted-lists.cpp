037f58ee83bef853765f5fde027c65d524945f54
Vyshnavi-d-p-3 <vyshnavi.dyvandinnepullareddy@gmail.com>
2025-08-01 20:35:36 +0530

    Time: 0 ms (100.00%) | Memory: 18.7 MB (41.21%) - LeetSync

--- 23-merge-k-sorted-lists/merge-k-sorted-lists.cpp
+++ 23-merge-k-sorted-lists/merge-k-sorted-lists.cpp
@@ -12,64 +12,25 @@ class Solution {
 public:
     ListNode* mergeKLists(vector<ListNode*>& lists) {
         // Approach 1 : Using Priority Queue , TC : klogk + N*k*logk, SC : O(k)
-        // priority_queue<pair<int, ListNode*>,vector<pair<int, ListNode*>>, greater<pair<int, ListNode*>>> pq;
-        // for(int i = 0; i < lists.size(); i++) {
-        //     if(lists[i]) {
-        //         pq.push({lists[i]->val, lists[i]});
-        //     }
-        // }
-
-        // ListNode* dummyNode = new ListNode(-1);
-        // ListNode* temp = dummyNode;
-        // while(!pq.empty()) {
-        //     auto it = pq.top();
-        //     pq.pop();
-        //     if(it.second->next) {
-        //         pq.push({it.second->next->val, it.second->next});
-        //     }
-        //     temp->next = it.second;
-        //     temp = temp -> next;
-        // }
-        // return dummyNode->next;
-
-        if (lists.empty()) {
-            return nullptr;
-        }
-        return mergeKListsHelper(lists, 0, lists.size() - 1);
-    }
-    
-    ListNode* mergeKListsHelper(vector<ListNode*>& lists, int start, int end) {
-        if (start == end) {
-            return lists[start];
-        }
-        if (start + 1 == end) {
-            return merge(lists[start], lists[end]);
-        }
-        int mid = start + (end - start) / 2;
-        ListNode* left = mergeKListsHelper(lists, start, mid);
-        ListNode* right = mergeKListsHelper(lists, mid + 1, end);
-        return merge(left, right);
-    }
-    
-    ListNode* merge(ListNode* l1, ListNode* l2) {
-        ListNode* dummy = new ListNode(0);
-        ListNode* curr = dummy;
-        
-        while (l1 && l2) {
-            if (l1->val < l2->val) {
-                curr->next = l1;
-                l1 = l1->next;
-            } else {
-                curr->next = l2;
-                l2 = l2->next;
+        priority_queue<pair<int, ListNode*>,vector<pair<int, ListNode*>>, greater<pair<int, ListNode*>>> pq;
+        for(int k = 0; k < lists.size(); k++) {
+            if(lists[k]) {
+                pq.push({lists[k]->val, lists[k]});
             }
-            curr = curr->next;
         }
-        
-        curr->next = l1 ? l1 : l2;
-        
-        return dummy->next;
 
-        
+        ListNode* dummyNode = new ListNode(-1);
+        ListNode* temp = dummyNode;
+        while(!pq.empty()) {
+            auto it = pq.top();
+            pq.pop();
+            if(it.second->next) {
+                pq.push({it.second->next->val, it.second->next});
+            }
+            temp->next = it.second;
+            temp = temp -> next;
+        }
+        return dummyNode->next;
+
     }
 };
