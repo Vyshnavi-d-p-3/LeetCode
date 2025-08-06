db1279c449126b65529b40e3bdfab3913dc656dd
Vyshnavi-d-p-3 <vyshnavi.dyvandinnepullareddy@gmail.com>
2025-08-06 13:25:27 +0530

    Time: 0 ms (100.00%) | Memory: 16.6 MB (97.58%) - LeetSync

--- 102-binary-tree-level-order-traversal/binary-tree-level-order-traversal.cpp
+++ 102-binary-tree-level-order-traversal/binary-tree-level-order-traversal.cpp
@@ -12,17 +12,39 @@
 class Solution {
 public:
     // Approach 1 : Recursive Approach
-    vector<vector<int>> levels;
-    void helper(TreeNode* node, int level) {
-        if(levels.size() == level) 
-            levels.push_back(vector<int>());
-        levels[level].push_back(node->val);
-        if(node->left != nullptr) helper(node->left, level + 1);
-        if(node->right != nullptr) helper(node->right, level + 1);
-    }
-    vector<vector<int>> levelOrder(TreeNode* root) {
-        if(root == nullptr) return levels;
-        helper(root, 0);
+    // vector<vector<int>> levels;
+    // void helper(TreeNode* node, int level) {
+    //     if(levels.size() == level) 
+    //         levels.push_back(vector<int>());
+    //     levels[level].push_back(node->val);
+    //     if(node->left != nullptr) helper(node->left, level + 1);
+    //     if(node->right != nullptr) helper(node->right, level + 1);
+    // }
+    // vector<vector<int>> levelOrder(TreeNode* root) {
+    //     if(root == nullptr) return levels;
+    //     helper(root, 0);
+    //     return levels;
+    // }
+
+    // Approach 2 : Iterative Approach
+    vector<vector<int>> levelOrder(TreeNode* root){
+        vector<vector<int>> levels;
+        if(root == NULL) return levels;
+        deque<TreeNode*> queue;
+        queue.push_back(root);
+        int level = 0;
+        while(!queue.empty()) {
+            levels.push_back({});
+            int level_length = queue.size();
+            for(int i = 0; i < level_length; ++i) {
+                TreeNode* node = queue.front();
+                queue.pop_front();
+                levels[level].push_back(node->val);
+                if(node->left != NULL) queue.push_back(node->left);
+                if(node->right != NULL) queue.push_back(node->right);
+            }
+            level++;
+        }
         return levels;
     }
 };
