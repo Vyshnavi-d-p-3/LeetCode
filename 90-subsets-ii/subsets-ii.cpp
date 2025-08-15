981a239aa421824577790c7744a4f228547743d9
Vyshu <vyshnavi.dyvandinnepullareddy@gmail.com>
2025-08-15 05:46:42 +0530

    Time: 5 ms (12.97%) | Memory: 12.3 MB (18.64%) - LeetSync

---
+++ 90-subsets-ii/subsets-ii.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    vector<vector<int>> subsetsWithDup(vector<int>& nums) {
+        int n = nums.size();
+        // Sort the generated subset. This will help to identify duplicates
+        sort(nums.begin(), nums.end());
+        vector<vector<int>> subsets;
+        int maxNumberOfSubsets = pow(2, n);
+        // To store the previously seen sets.
+        unordered_set<string> seen;
+        for(int subsetIndex = 0; subsetIndex < maxNumberOfSubsets; subsetIndex++) {
+            // Append the subset corresponding to that bitmask
+            vector<int> currentSubset;
+            string hashcode = "";
+            for(int j = 0; j < n; j++) {
+                // Generate the bitmask
+                int mask = 1 << j;
+                int isSet = mask & subsetIndex;
+                if(isSet != 0) {
+                    currentSubset.push_back(nums[j]);
+                    // Generating the hascode by creating a comma seperated
+                    // string of numbers in the currentSubset.
+                    hashcode.append(to_string(nums[j]) + ',');
+                }
+            }
+            if(seen.find(hashcode) == seen.end()) {
+                subsets.push_back(currentSubset);
+                seen.insert(hashcode);
+            }
+        }
+        return subsets;
+    }
+};
