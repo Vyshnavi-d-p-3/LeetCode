774c6b2a019e590c34846f5835a2011914ae00c8
Vyshu <vyshnavi.dyvandinnepullareddy@gmail.com>
2025-08-23 08:01:27 +0530

    Time: 69 ms (44.72%) | Memory: 148.6 MB (17.28%) - LeetSync

--- 295-find-median-from-data-stream/find-median-from-data-stream.cpp
+++ 295-find-median-from-data-stream/find-median-from-data-stream.cpp
@@ -1,18 +1,27 @@
 class MedianFinder {
-    vector<int> store;
+    priority_queue<int> lo; // Max Heap
+    priority_queue<int, vector<int>, greater<int>> hi; // Min Heap
 public:
     MedianFinder() {
         
     }
     
     void addNum(int num) {
-        if(store.empty()) store.push_back(num);
-        else store.insert(lower_bound(store.begin(), store.end(), num), num);
+        // if(store.empty()) store.push_back(num);
+        // else store.insert(lower_bound(store.begin(), store.end(), num), num);
+        lo.push(num);
+        hi.push(lo.top());
+        lo.pop();
+        if(lo.size() < hi.size()) {
+            lo.push(hi.top());
+            hi.pop();
+        }
     }
-    
+        // Return the median of current data stream
     double findMedian() {
-        int n = store.size();
-        return n & 1 ? store[n / 2] : ((double) store[n / 2 - 1] + store[n / 2]) * 0.5;
+        // int n = store.size();
+        // return n & 1 ? store[n / 2] : ((double) store[n / 2 - 1] + store[n / 2]) * 0.5;
+        return lo.size() > hi.size() ? lo.top() : ((double) lo.top() + hi.top()) * 0.5;
     }
 };
 
