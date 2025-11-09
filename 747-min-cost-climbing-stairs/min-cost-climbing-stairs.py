4404743ed6289fa80ee16da7a221f45352923498
Vyshu <vyshnavi.dyvandinnepullareddy@gmail.com>
2025-11-08 18:45:56 -0800

    Time: 4 ms (38.45%) | Memory: 20.7 MB (7.53%) - LeetSync

--- 747-min-cost-climbing-stairs/min-cost-climbing-stairs.py
+++ 747-min-cost-climbing-stairs/min-cost-climbing-stairs.py
@@ -21,20 +21,34 @@
 
 
 # Top Down Approach
+# class Solution:
+#     def minCostClimbingStairs(self, cost: List[int]) -> int:
+#         def minimum_cost(i):
+#             if i <= 1:
+#                 return 0
+#             if i in memo:
+#                 return memo[i]
+
+#             down_one = cost[i - 1] + minimum_cost(i - 1)
+#             down_two = cost[i - 2] + minimum_cost(i - 2)
+#             memo[i] = min(down_one, down_two)
+#             return memo[i]
+#         memo = {}
+#         return minimum_cost(len(cost))
+
+# Automatic Memoization available functools Modules in Python
+
 class Solution:
     def minCostClimbingStairs(self, cost: List[int]) -> int:
+        @cache
         def minimum_cost(i):
-            if i <= 1:
+            if i<= 1:
                 return 0
-            if i in memo:
-                return memo[i]
-
             down_one = cost[i - 1] + minimum_cost(i - 1)
             down_two = cost[i - 2] + minimum_cost(i - 2)
-            memo[i] = min(down_one, down_two)
-            return memo[i]
-        memo = {}
+            return min(down_one, down_two)
         return minimum_cost(len(cost))
+
 
 
        
