5ebfb94cc98ab4d850ae9f36878f9521eb07dd76
Vyshu <vyshnavi.dyvandinnepullareddy@gmail.com>
2025-11-08 18:19:04 -0800

    Time: 0 ms (100.00%) | Memory: 17.9 MB (63.13%) - LeetSync

--- 747-min-cost-climbing-stairs/min-cost-climbing-stairs.py
+++ 747-min-cost-climbing-stairs/min-cost-climbing-stairs.py
@@ -8,22 +8,29 @@
 #             minimum_cost[i] = min(take_one_step, take_two_steps)
 #         return minimum_cost[-1]
 
-
-# Top Down Approach
 class Solution:
     def minCostClimbingStairs(self, cost: List[int]) -> int:
-        def minimum_cost(i):
-            if i <= 1:
-                return 0
-            if i in memo:
-                return memo[i]
+        cost.append(0)
+        for i  in range(len(cost) - 3, -1, -1):
+            cost[i] += min(cost[i+1], cost[i+2])
 
-            down_one = cost[i - 1] + minimum_cost(i - 1)
-            down_two = cost[i - 2] + minimum_cost(i - 2)
-            memo[i] = min(down_one, down_two)
-            return memo[i]
-        memo = {}
-        return minimum_cost(len(cost))
+        return min(cost[0], cost[1])
+
+# Top Down Approach
+# class Solution:
+#     def minCostClimbingStairs(self, cost: List[int]) -> int:
+#         def minimum_cost(i):
+#             if i <= 1:
+#                 return 0
+#             if i in memo:
+#                 return memo[i]
+
+#             down_one = cost[i - 1] + minimum_cost(i - 1)
+#             down_two = cost[i - 2] + minimum_cost(i - 2)
+#             memo[i] = min(down_one, down_two)
+#             return memo[i]
+#         memo = {}
+#         return minimum_cost(len(cost))
 
 
        
