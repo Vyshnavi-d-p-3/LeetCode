509bf96652633aa7d7e61b852becf771fe66e9de
Vyshu <vyshnavi.dyvandinnepullareddy@gmail.com>
2025-11-09 20:10:07 -0800

    Time: 0 ms (100.00%) | Memory: 17.7 MB (48.34%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.py
+++ 70-climbing-stairs/climbing-stairs.py
@@ -28,13 +28,26 @@
 #         return memo[i]
 
 # Optimal - Dynamic Programming - TC(O(n)) - SC(O(n))
+# class Solution:
+#     def climbStairs(self, n: int) -> int:
+#         if n == 1:
+#             return 1
+#         dp = [0 for _ in range(n+1)]
+#         dp[1] = 1
+#         dp[2] = 2
+#         for i in range(3, n+1):
+#             dp[i] = dp[i - 1] + dp[i - 2]
+#         return dp[n]
+
+# Optimal - Fibonacci Number
 class Solution:
-    def climbStairs(self, n: int) -> int:
+    def climbStairs(self, n):
         if n == 1:
             return 1
-        dp = [0 for _ in range(n+1)]
-        dp[1] = 1
-        dp[2] = 2
-        for i in range(3, n+1):
-            dp[i] = dp[i - 1] + dp[i - 2]
-        return dp[n]
+        first = 1
+        second = 2
+        for i in range(3, n + 1):
+            third = first + second
+            first = second
+            second = third
+        return second
