40f2a3fbbedc6e38616fa32354a651277e261fad
Vyshu <vyshnavi.dyvandinnepullareddy@gmail.com>
2025-11-09 20:02:03 -0800

    Time: 0 ms (100.00%) | Memory: 17.9 MB (8.11%) - LeetSync

---
+++ 70-climbing-stairs/climbing-stairs.py
@@ -0,0 +1,28 @@
+# BruteForce - Recursion - TC(O(2^n)) - SC(O(n))
+# class Solution:
+#     def climbStairs(self, n: int) -> int:
+#         return self.climb_Stairs(0, n)
+
+#     def climb_Stairs(self, i: int, n: int) -> int:
+#         if i > n:
+#             return 0
+#         if i == n:
+#             return 1
+#         return self.climb_Stairs(i + 1, n) + self.climb_Stairs(i + 2, n)
+
+# Better - Recursion with Memo - TC(O(n)) - SC(O(n))
+class Solution:
+    def climbStairs(self, n:int) -> int:
+        memo = [0] * (n+1)
+        return self.climb_Stairs(0, n, memo)
+
+    def climb_Stairs(self, i: int, n: int, memo: List[int]) -> int:
+        if i > n:
+            return 0
+        if i == n:
+            return 1
+        if memo[i] > 0:
+            return memo[i]
+        memo[i] = self.climb_Stairs(i+1, n, memo) + self.climb_Stairs(i+2, n, memo)
+
+        return memo[i]
