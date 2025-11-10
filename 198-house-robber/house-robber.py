39ef93a0fa29aea76cfd9d8940d7725727f48669
Vyshu <vyshnavi.dyvandinnepullareddy@gmail.com>
2025-11-10 08:31:02 -0800

    Time: 0 ms (100.00%) | Memory: 17.9 MB (33.76%) - LeetSync

--- 198-house-robber/house-robber.py
+++ 198-house-robber/house-robber.py
@@ -1,20 +1,14 @@
 class Solution:
     def __init__(self):
         self.memo = {}
-
     def rob(self, nums: List[int]) -> int:
         self.memo = {}
-        return self.robForm(0, nums)
-    def robForm(self, i, nums):
-        # No more houses left to examine.
-        if i >= len(nums):
+        return self.robFrom(0, nums)
+    def robFrom(self, i, nums):
+        if i>= len(nums):
             return 0
-        # REturn cached value.
         if i in self.memo:
             return self.memo[i]
-        # Recursive relation evaluation to get the optimal answer.
-        ans = max(self.robForm(i + 1, nums), self.robForm(i + 2, nums) + nums[i])
-
-        # Cache for future use
+        ans = max(self.robFrom(i + 1, nums), self.robFrom(i + 2, nums) + nums[i])
         self.memo[i] = ans
         return ans
