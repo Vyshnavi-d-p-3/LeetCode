9672bd2124ee7e8349e4f427f163013a50843014
Vyshu <vyshnavi.dyvandinnepullareddy@gmail.com>
2025-11-09 20:06:03 -0800

    Time: 0 ms (100.00%) | Memory: 17.5 MB (95.56%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.py
+++ 70-climbing-stairs/climbing-stairs.py
@@ -11,18 +11,30 @@
 #         return self.climb_Stairs(i + 1, n) + self.climb_Stairs(i + 2, n)
 
 # Better - Recursion with Memo - TC(O(n)) - SC(O(n))
+# class Solution:
+#     def climbStairs(self, n:int) -> int:
+#         memo = [0] * (n+1)
+#         return self.climb_Stairs(0, n, memo)
+
+#     def climb_Stairs(self, i: int, n: int, memo: List[int]) -> int:
+#         if i > n:
+#             return 0
+#         if i == n:
+#             return 1
+#         if memo[i] > 0:
+#             return memo[i]
+#         memo[i] = self.climb_Stairs(i+1, n, memo) + self.climb_Stairs(i+2, n, memo)
+
+#         return memo[i]
+
+# Optimal - Dynamic Programming 
 class Solution:
-    def climbStairs(self, n:int) -> int:
-        memo = [0] * (n+1)
-        return self.climb_Stairs(0, n, memo)
-
-    def climb_Stairs(self, i: int, n: int, memo: List[int]) -> int:
-        if i > n:
-            return 0
-        if i == n:
+    def climbStairs(self, n: int) -> int:
+        if n == 1:
             return 1
-        if memo[i] > 0:
-            return memo[i]
-        memo[i] = self.climb_Stairs(i+1, n, memo) + self.climb_Stairs(i+2, n, memo)
-
-        return memo[i]
+        dp = [0 for _ in range(n+1)]
+        dp[1] = 1
+        dp[2] = 2
+        for i in range(3, n+1):
+            dp[i] = dp[i - 1] + dp[i - 2]
+        return dp[n]
