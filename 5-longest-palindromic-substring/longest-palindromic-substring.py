5ccb379a8c318560720b048547b8477225051849
Vyshu <vyshnavi.dyvandinnepullareddy@gmail.com>
2025-11-13 17:22:35 -0800

    Time: 1995 ms (24.27%) | Memory: 25.5 MB (8.26%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.py
+++ 5-longest-palindromic-substring/longest-palindromic-substring.py
@@ -1,21 +1,46 @@
+# class Solution:
+#     def longestPalindrome(self, s: str) -> str:
+#         def check(i, j):
+#             left = i
+#             right = j - 1
+
+#             while left < right:
+#                 if s[left] != s[right]:
+#                     return False
+
+#                 left += 1
+#                 right -= 1
+
+#             return True
+
+#         for length in range(len(s), 0, -1):
+#             for start in range(len(s) - length + 1):
+#                 if check(start, start + length):
+#                     return s[start : start + length]
+
+#         return ""
+
+# Dynamic Programming
 class Solution:
-    def longestPalindrome(self, s: str) -> str:
-        def check(i, j):
-            left = i
-            right = j - 1
+    def longestPalindrome(self, s:str) -> str:
+        n = len(s)
+        dp = [[False] * n for _ in range(n)]
+        ans = [0, 0]
 
-            while left < right:
-                if s[left] != s[right]:
-                    return False
+        for i in range(n):
+            dp[i][i] = True
 
-                left += 1
-                right -= 1
+        for i in range(n - 1):
+            if s[i] == s[i + 1]:
+                dp[i][i + 1] = True
+                ans = [i, i+1]
 
-            return True
+        for diff in range(2, n):
+            for i in range(n - diff):
+                j = i + diff
+                if s[i] == s[j] and dp[i + 1][j - 1]:
+                    dp[i][j] = True
+                    ans = [i, j]
 
-        for length in range(len(s), 0, -1):
-            for start in range(len(s) - length + 1):
-                if check(start, start + length):
-                    return s[start : start + length]
-
-        return ""
+        i, j = ans
+        return s[i : j + 1]
