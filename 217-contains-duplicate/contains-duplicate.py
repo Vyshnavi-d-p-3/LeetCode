b2077845b0ee40eeef6fa176af1e12c0e27ba09e
Vyshu <vyshnavi.dyvandinnepullareddy@gmail.com>
2026-01-13 19:58:56 -0800

    Time: 22 ms (19.74%) | Memory: 36.4 MB (5.82%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.py
+++ 217-contains-duplicate/contains-duplicate.py
@@ -1,8 +1,40 @@
+# Brute Force : TC(O(N^2))
+# class Solution:
+#     def containsDuplicate(self, nums: List[int]) -> bool:
+#         n = len(nums)
+#         for i in range(n - 1):
+#             for j in range(i + 1, n):
+#                 if nums[i] == nums[j]:
+#                     return True
+#         return False
+
+
+# Sorting the array and searching : TC(O(NlogN))
+# class Solution:
+#     def containsDuplicate(self, nums: List[int]) -> bool:
+#         nums.sort()
+#         n = len(nums)
+#         for i in range(1, n):
+#             if nums[i] == nums[i-1]:
+#                 return True
+#         return False
+
+# Hash Set : TC(O(N))
+# class Solution:
+#     def containsDuplciate(self, nums: List[int]) -> bool:
+#         seen = set()
+#         for num in nums:
+#             if num in seen:
+#                 return True
+#             seen.add(num)
+#         return False
+
+# Hash Map : TC(O(N))
 class Solution:
-    def containsDuplicate(self, nums: List[int]) -> bool:
-        nums.sort()
-        n = len(nums)
-        for i in range(1, n):
-            if nums[i] == nums[i-1]:
+    def containsDuplicate(Self, nums: List[int]) -> bool:
+        seen = {}
+        for num in nums:
+            if num in seen and seen[num] >= 1:
                 return True
+            seen[num] = seen.get(num, 0) + 1
         return False
