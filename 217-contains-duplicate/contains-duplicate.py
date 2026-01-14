c834b5aeeca2eced8b3acac5b8e305c8e05290b2
Vyshu <vyshnavi.dyvandinnepullareddy@gmail.com>
2026-01-13 20:11:22 -0800

    Time: 7 ms (91.41%) | Memory: 31.2 MB (67.77%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.py
+++ 217-contains-duplicate/contains-duplicate.py
@@ -20,14 +20,14 @@
 #         return False
 
 # Hash Set : TC(O(N))
-# class Solution:
-#     def containsDuplicate(self, nums: List[int]) -> bool:
-#         List_NoDuplicates = set()
-#         for num in nums:
-#             if num in List_NoDuplicates:
-#                 return True
-#             List_NoDuplicates.add(num)
-#         return False
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        List_NoDuplicates = set()
+        for num in nums:
+            if num in List_NoDuplicates:
+                return True
+            List_NoDuplicates.add(num)
+        return False
 
 # Hash Map : TC(O(N))
 # class Solution:
@@ -40,6 +40,6 @@
 #         return False
 
 # Short Form - Hash Set - TC(O(n)), SC(O(n))
-class Solution:
-    def containsDuplicate(self, nums:List[int]) -> bool:
-        return True if len(set(nums)) < len(nums) else False
+# class Solution:
+#     def containsDuplicate(self, nums:List[int]) -> bool:
+#         return True if len(set(nums)) < len(nums) else False
