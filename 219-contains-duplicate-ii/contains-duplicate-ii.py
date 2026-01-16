bf0bb7800e8a79fdebfc50bf6a2d3af64572940c
Vyshu <vyshnavi.dyvandinnepullareddy@gmail.com>
2026-01-16 05:44:49 -0800

    Time: 33 ms (43.90%) | Memory: 36.6 MB (29.30%) - LeetSync

--- 219-contains-duplicate-ii/contains-duplicate-ii.py
+++ 219-contains-duplicate-ii/contains-duplicate-ii.py
@@ -9,15 +9,27 @@
 #                 seen[val] = i
 #         return False
 
+# class Solution:
+#     def containsNearbyDuplicate(self, nums: List[int], k: int) -> bool:
+#         left = 0
+#         window = set()
+#         for right in range(len(nums)):
+#             if nums[right] in window:
+#                 return True
+#             window.add(nums[right])
+#             if right - left >= k:
+#                 window.remove(nums[left])
+#                 left += 1
+#         return False
+
+
+# HashMap
 class Solution:
     def containsNearbyDuplicate(self, nums: List[int], k: int) -> bool:
-        left = 0
-        window = set()
-        for right in range(len(nums)):
-            if nums[right] in window:
+        seen = {}
+        for i, val in enumerate(nums):
+            if val in seen and i - seen[val] <= k:
                 return True
-            window.add(nums[right])
-            if right - left >= k:
-                window.remove(nums[left])
-                left += 1
+            else:
+                seen[val] = i
         return False
