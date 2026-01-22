ef4c75696bf0c4a4827b37bf3f47e64840ee171e
Vyshu <vyshnavi.dyvandinnepullareddy@gmail.com>
2026-01-21 18:14:16 -0800

    Time: 12 ms (59.83%) | Memory: 24 MB (7.16%) - LeetSync

--- 49-group-anagrams/group-anagrams.py
+++ 49-group-anagrams/group-anagrams.py
@@ -1,6 +1,16 @@
+# class Solution:
+#     def groupAnagrams(self,strs: List[str]) -> List[List[str]]:
+#         ans = collections.defaultdict(list)
+#         for s in strs:
+#             ans[tuple(sorted(s))].append(s)
+#         return list(ans.values())
+
 class Solution:
-    def groupAnagrams(self,strs: List[str]) -> List[List[str]]:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
         ans = collections.defaultdict(list)
         for s in strs:
-            ans[tuple(sorted(s))].append(s)
+            count = [0] * 26
+            for c in s:
+                count[ord(c) - ord("a")] += 1
+            ans[tuple(count)].append(s)
         return list(ans.values())
