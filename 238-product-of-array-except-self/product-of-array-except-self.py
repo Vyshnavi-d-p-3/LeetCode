3cd6677bb33112e9fbbcb6695a5c996296bf996f
Vyshu <vyshnavi.dyvandinnepullareddy@gmail.com>
2026-01-21 21:03:30 -0800

    Time: 25 ms (50.68%) | Memory: 25.8 MB (28.55%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.py
+++ 238-product-of-array-except-self/product-of-array-except-self.py
@@ -11,20 +11,36 @@ class Solution:
             postfix *= nums[i]
         return res
 
+# TC(O(N)), SC(O(1))
+class Solution:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        length = len(nums)
+        ans = [0] * length
+        ans[0] = 1
+        for i in range(1, length):
+            ans[i] = ans[i - 1] * nums[i - 1]
 
-    class Solution:
-        def productExceptSelf(self, nums: List[int]) -> List[int]:
-            length = len(nums)
-            L, R, answer =[0]* length, [0] * length, [0] * length
-            L[0] = 1
-            for i in range(1, length):
-                L[i] = L[i - 1] * nums[i - 1]
+        R = 1
+        for i in reversed(range(length)):
+            ans[i] *= R
+            R *= nums[i]
 
-            R[length - 1] = 1
-            for i in reversed(range(length - 1)):
-                R[i] = R[i + 1] * nums[i + 1]
+        return ans
 
-            for i in range(length):
-                answer[i] = L[i] * R[i]
+# TC(O(N)), SC(O(N))
+    # class Solution:
+    #     def productExceptSelf(self, nums: List[int]) -> List[int]:
+    #         length = len(nums)
+    #         L, R, answer =[0]* length, [0] * length, [0] * length
+    #         L[0] = 1
+    #         for i in range(1, length):
+    #             L[i] = L[i - 1] * nums[i - 1]
+
+    #         R[length - 1] = 1
+    #         for i in reversed(range(length - 1)):
+    #             R[i] = R[i + 1] * nums[i + 1]
+
+    #         for i in range(length):
+    #             answer[i] = L[i] * R[i]
             
-            return answer
+    #         return answer
