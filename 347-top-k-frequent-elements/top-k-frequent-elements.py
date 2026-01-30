c3277c58baad75598fe74bb69e38a24f51ff0be9
Vyshu <vyshnavi.dyvandinnepullareddy@gmail.com>
2026-01-30 07:48:11 -0800

    Time: 4 ms (70.77%) | Memory: 22.8 MB (43.55%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.py
+++ 347-top-k-frequent-elements/top-k-frequent-elements.py
@@ -17,5 +17,6 @@ class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
         if k == len(nums):
             return nums
+
         count = Counter(nums)
         return heapq.nlargest(k, count.keys(), key = count.get)
