aa085b998764c413de587bebd73a541ed1c0b573
Vyshu <vyshnavi.dyvandinnepullareddy@gmail.com>
2026-02-02 16:37:16 -0800

    Time: 51 ms (47.40%) | Memory: 30.9 MB (52.09%) - LeetSync

--- 41-first-missing-positive/first-missing-positive.py
+++ 41-first-missing-positive/first-missing-positive.py
@@ -19,16 +19,37 @@
 
 #         return ans
 
+# Approach: Using an array to store whether the element is present or not, TC: O(n) ,SC: O(n)
+# class Solution:
+#     def firstMissingPositive(self, nums):
+#         n = len(nums)
+#         seen = [False] * (n + 1)
+
+#         for num in nums:
+#             if 0 < num <= n:
+#                 seen[num] = True
+
+#         for i in range(1, n+1):
+#             if not seen[i]:
+#                 return i
+#         return n + 1
+
+# Approach: Array Manipulation - TC: O(), SC: O()
 class Solution:
-    def firstMissingPositive(self, nums):
+    def firstMissingPositive(self, nums: List[int]) -> int:
+        def swap(arr, i, j):
+            arr[i], arr[j] = arr[j], arr[i]
+        
         n = len(nums)
-        seen = [False] * (n + 1)
 
-        for num in nums:
-            if 0 < num <= n:
-                seen[num] = True
+        # Place each positive integer i at index i - 1 if possible
+        for i in range(n):
+            while  0 < nums[i] <= n and nums[i] != nums[nums[i] - 1]:
+                swap(nums, i, nums[i] - 1)
 
-        for i in range(1, n+1):
-            if not seen[i]:
-                return i
+        for i in range(n):
+            if nums[i] != i + 1:
+                return i + 1
+
         return n + 1
+    
