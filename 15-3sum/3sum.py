dcdfd7baaa658d3740e8e45cf28970898ae50b68
Vyshu <vyshnavi.dyvandinnepullareddy@gmail.com>
2026-02-12 13:10:13 -0800

    Time: 521 ms (78.73%) | Memory: 22.4 MB (22.61%) - LeetSync

--- 15-3sum/3sum.py
+++ 15-3sum/3sum.py
@@ -5,10 +5,9 @@ class Solution:
         for i in range(len(nums)):
             if nums[i] > 0:
                 break
-            if i == 0 or nums[i - 1] != nums[i]:
+            if i == 0 or nums[i-1] != nums[i]:
                 self.twoSumII(nums, i, res)
         return res
-
     def twoSumII(self, nums: List[int], i: int, res: List[List[int]]):
         lo, hi = i + 1, len(nums) - 1
         while lo < hi:
