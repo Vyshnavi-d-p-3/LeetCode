0b517bcbb1bcb51234ffe047bfc39ff26cf0b315
Vyshu <vyshnavi.dyvandinnepullareddy@gmail.com>
2026-03-17 12:44:55 -0700

    Time: 3 ms (21.33%) | Memory: 19.4 MB (95.56%) - LeetSync

--- 412-fizz-buzz/fizz-buzz.py
+++ 412-fizz-buzz/fizz-buzz.py
@@ -1,16 +1,33 @@
+# class Solution:
+#     def fizzBuzz(self, n: int) -> List[str]:
+#         ans = []
+#         for num in range(1, n+1):
+#             divisible_by_3 = (num % 3 == 0)
+#             divisible_by_5 = (num % 5 == 0)
+
+#             if divisible_by_3 and divisible_by_5:
+#                 ans.append("FizzBuzz")
+#             elif divisible_by_3:
+#                 ans.append("Fizz")
+#             elif divisible_by_5:
+#                 ans.append("Buzz")
+#             else:
+#                 ans.append(str(num))
+#         return ans
+
 class Solution:
     def fizzBuzz(self, n: int) -> List[str]:
         ans = []
-        for num in range(1, n+1):
+        for num in range(1, n + 1):
             divisible_by_3 = (num % 3 == 0)
             divisible_by_5 = (num % 5 == 0)
+            nums_ans_str = ""
+            if divisible_by_3:
+                nums_ans_str += "Fizz"
+            if divisible_by_5:
+                nums_ans_str += "Buzz"
+            if not nums_ans_str:
+                nums_ans_str = str(num)
+            ans.append(nums_ans_str)
 
-            if divisible_by_3 and divisible_by_5:
-                ans.append("FizzBuzz")
-            elif divisible_by_3:
-                ans.append("Fizz")
-            elif divisible_by_5:
-                ans.append("Buzz")
-            else:
-                ans.append(str(num))
         return ans
