8442a61c7f4142e49c77be13e7d8087a94fe037c
Vyshu <vyshnavi.dyvandinnepullareddy@gmail.com>
2026-03-17 12:55:12 -0700

    Time: 1 ms (34.43%) | Memory: 19.7 MB (46.25%) - LeetSync

--- 412-fizz-buzz/fizz-buzz.py
+++ 412-fizz-buzz/fizz-buzz.py
@@ -15,19 +15,38 @@
 #                 ans.append(str(num))
 #         return ans
 
+# class Solution:
+#     def fizzBuzz(self, n: int) -> List[str]:
+#         ans = []
+#         for num in range(1, n + 1):
+#             divisible_by_3 = (num % 3 == 0)
+#             divisible_by_5 = (num % 5 == 0)
+#             nums_ans_str = ""
+#             if divisible_by_3:
+#                 nums_ans_str += "Fizz"
+#             if divisible_by_5:
+#                 nums_ans_str += "Buzz"
+#             if not nums_ans_str:
+#                 nums_ans_str = str(num)
+#             ans.append(nums_ans_str)
+
+#         return ans
+
 class Solution:
     def fizzBuzz(self, n: int) -> List[str]:
         ans = []
-        for num in range(1, n + 1):
-            divisible_by_3 = (num % 3 == 0)
-            divisible_by_5 = (num % 5 == 0)
-            nums_ans_str = ""
-            if divisible_by_3:
-                nums_ans_str += "Fizz"
-            if divisible_by_5:
-                nums_ans_str += "Buzz"
-            if not nums_ans_str:
-                nums_ans_str = str(num)
-            ans.append(nums_ans_str)
 
+        fizz_buzz_dict = {3 : "Fizz", 5 : "Buzz"}
+        divisors = [3, 5]
+
+        for num in range(1, n + 1):
+            nums_ans_str = []
+            for key in divisors:
+                if num % key == 0:
+                    nums_ans_str.append(fizz_buzz_dict[key])
+
+            if not nums_ans_str:
+                nums_ans_str.append(str(num))
+
+            ans.append(''.join(nums_ans_str))
         return ans
