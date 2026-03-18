fed95fff67f8568addd1c1128c50cedf0d0a3fd1
Vyshu <vyshnavi.dyvandinnepullareddy@gmail.com>
2026-03-17 17:46:57 -0700

    Time: 3 ms (22.09%) | Memory: 19.2 MB (89.61%) - LeetSync

--- 14-longest-common-prefix/longest-common-prefix.py
+++ 14-longest-common-prefix/longest-common-prefix.py
@@ -1,11 +1,23 @@
+# class Solution:
+#     def longestCommonPrefix(self, strs: List[str]) -> str:
+#         if len(strs) == 0:
+#             return ""
+#         prefix = strs[0]
+#         for i in range(1, len(strs)):
+#             while strs[i].find(prefix) !=0:
+#                 prefix = prefix[0 : len(prefix) - 1]
+#                 if prefix == "":
+#                     return ""
+#         return prefix
+
+# Vertical Scanning
 class Solution:
     def longestCommonPrefix(self, strs: List[str]) -> str:
-        if len(strs) == 0:
+        if strs == None or len(strs) == 0:
             return ""
-        prefix = strs[0]
-        for i in range(1, len(strs)):
-            while strs[i].find(prefix) !=0:
-                prefix = prefix[0 : len(prefix) - 1]
-                if prefix == "":
-                    return ""
-        return prefix
+        for i in range(len(strs[0])):
+            c = strs[0][i]
+            for j in range(1, len(strs)):
+                if i == len(strs[j]) or strs[j][i] != c:
+                    return strs[0][0:i]
+        return strs[0]
