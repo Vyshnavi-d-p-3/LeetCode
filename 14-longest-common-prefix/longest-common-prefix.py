3f25ff3f7f99dd0420d54c3f7d343621f1a2eeb8
Vyshu <vyshnavi.dyvandinnepullareddy@gmail.com>
2026-03-17 20:09:30 -0700

    Time: 0 ms (100.00%) | Memory: 19.5 MB (26.61%) - LeetSync

--- 14-longest-common-prefix/longest-common-prefix.py
+++ 14-longest-common-prefix/longest-common-prefix.py
@@ -11,13 +11,40 @@
 #         return prefix
 
 # Vertical Scanning
+# class Solution:
+#     def longestCommonPrefix(self, strs: List[str]) -> str:
+#         if strs == None or len(strs) == 0:
+#             return ""
+#         for i in range(len(strs[0])):
+#             c = strs[0][i]
+#             for j in range(1, len(strs)):
+#                 if i == len(strs[j]) or strs[j][i] != c:
+#                     return strs[0][0:i]
+#         return strs[0]
+
+# Divide & Conquere
 class Solution:
-    def longestCommonPrefix(self, strs: List[str]) -> str:
-        if strs == None or len(strs) == 0:
+    def longestCommonPrefix(self, strs):
+        if not strs:
             return ""
-        for i in range(len(strs[0])):
-            c = strs[0][i]
-            for j in range(1, len(strs)):
-                if i == len(strs[j]) or strs[j][i] != c:
-                    return strs[0][0:i]
-        return strs[0]
+
+        def LCP(left, right):
+            min_len = min(len(left), len(right))
+            for i in range(min_len):
+                if left[i] != right[i]:
+                    return left[:i]
+            return left[:min_len]
+
+        def divide_and_conquer(strs, l, r):
+            if l == r:
+                return strs[l]
+            else:
+                mid = (l + r) // 2
+                lcpLeft = divide_and_conquer(strs, l, mid)
+                lcpRight= divide_and_conquer(strs, mid + 1, r)
+
+                return LCP(lcpLeft, lcpRight)
+
+        return divide_and_conquer(strs, 0, len(strs) - 1)
+
+
