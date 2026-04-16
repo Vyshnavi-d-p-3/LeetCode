ee53374686784ed0a65670fa01e9a4be3d0c3c29
Vyshu <vyshnavi.dyvandinnepullareddy@gmail.com>
2026-04-16 12:41:45 -0700

    Time: 115 ms (18.36%) | Memory: 191.2 MB (5.36%) - LeetSync

--- 146-lru-cache/lru-cache.cpp
+++ 146-lru-cache/lru-cache.cpp
@@ -1,60 +1,101 @@
-struct Node {
-    int key;
-    int val;
-    Node *next;
-    Node *prev;
-    Node(int key, int val) : key(key), val(val), next(nullptr), prev(nullptr)
-    {}
-};
+// struct Node {
+//     int key;
+//     int val;
+//     Node *next;
+//     Node *prev;
+//     Node(int key, int val) : key(key), val(val), next(nullptr), prev(nullptr)
+//     {}
+// };
+
+// class LRUCache {
+//     public:
+//     int capacity;
+//     unordered_map<int, Node*> dic;
+//     Node *head = new Node(-1,-1);
+//     Node *tail = new Node(-1, -1);
+//     LRUCache(int capacity) {
+//         this->capacity = capacity;
+//         head->next = tail;
+//         tail->prev = head;
+//     }
+
+//     int get(int key) {
+//         if(dic.find(key) == dic.end()) {
+//             return -1;
+//     }
+//         Node *node = dic[key];
+//         remove(node);
+//         add(node);
+//         return node->val;
+//     }
+
+//     void put(int key, int value) {
+//         if(dic.find(key) != dic.end()){
+//             Node *oldNode = dic[key];
+//             remove(oldNode);
+//         }
+
+//         Node *node = new Node(key, value);
+//         dic[key] = node;
+//         add(node);
+//         if(dic.size() > capacity) {
+//             Node *nodeToDelete = head->next;
+//             remove(nodeToDelete);
+//             dic.erase(nodeToDelete->key);
+//         }
+//     }
+
+//     void add(Node *node){
+//         Node *previousEnd = tail->prev;
+//         previousEnd->next = node;
+//         node->prev = previousEnd;
+//         node->next = tail;
+//         tail->prev = node;
+//     }
+
+//     void remove(Node *node){
+//         node->prev->next = node->next;
+//         node->next->prev = node->prev;
+//     }
+// };
 
 class LRUCache {
-    public:
+public:
     int capacity;
-    unordered_map<int, Node*> dic;
-    Node *head = new Node(-1,-1);
-    Node *tail = new Node(-1, -1);
+    unordered_map<int, list<pair<int,int>>::iterator> dic;
+    list<pair<int, int>> lru;
     LRUCache(int capacity) {
         this->capacity = capacity;
-        head->next = tail;
-        tail->prev = head;
     }
 
     int get(int key) {
-        if(dic.find(key) == dic.end()) {
+        auto it = dic.find(key);
+        if(it == dic.end()){
             return -1;
-    }
-        Node *node = dic[key];
-        remove(node);
-        add(node);
-        return node->val;
+        }
+        int value = it->second->second;
+        lru.erase(it->second);
+        lru.push_front({key, value});
+
+        dic.erase(it);
+        dic[key] = lru.begin();
+        return value;
     }
 
-    void put(int key, int value) {
-        if(dic.find(key) != dic.end()){
-            Node *oldNode = dic[key];
-            remove(oldNode);
+    void put(int key,int value) {
+        auto it = dic.find(key);
+        if(dic.find(key) != dic.end()) {
+            lru.erase(it->second);
+            dic.erase(it);
         }
 
-        Node *node = new Node(key, value);
-        dic[key] = node;
-        add(node);
+        lru.push_front({key,value});
+        dic[key] = lru.begin();
+
         if(dic.size() > capacity) {
-            Node *nodeToDelete = head->next;
-            remove(nodeToDelete);
-            dic.erase(nodeToDelete->key);
+            auto it = dic.find(lru.rbegin()->first);
+            dic.erase(it);
+            lru.pop_back();
         }
-    }
-
-    void add(Node *node){
-        Node *previousEnd = tail->prev;
-        previousEnd->next = node;
-        node->prev = previousEnd;
-        node->next = tail;
-        tail->prev = node;
-    }
-
-    void remove(Node *node){
-        node->prev->next = node->next;
-        node->next->prev = node->prev;
     }
 };
