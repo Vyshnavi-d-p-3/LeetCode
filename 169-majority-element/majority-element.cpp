f3f26541568acdf144c1a9348d0469024bbc7b80
Vyshu <vyshnavi.dyvandinnepullareddy@gmail.com>
2026-04-18 15:26:10 -0700

    Time: 0 ms (100.00%) | Memory: 28.2 MB (27.77%) - LeetSync

--- 169-majority-element/majority-element.cpp
+++ 169-majority-element/majority-element.cpp
@@ -1,27 +1,48 @@
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
-        int el;
-        int count = 0;
-        for(int i = 0; i < nums.size(); i++){
-            if(count == 0) {
-                count = 1;
-                el = nums[i];
-            }else if(nums[i] == el) {
-                count++;
-            }else{
-                count--; 
+        unordered_map<int, int> counts;
+        for(int num: nums){
+            if(counts.find(num) == counts.end()){
+                counts[num] = 1;
+            }else {
+                counts[num]++;
             }
         }
-        int count1 = 0;
-        for(int i = 0; i < nums.size(); i++){
-            if(nums[i] == el){
-                count1++;
-            }
-        }
-        if(count1 > (nums.size()/2)) {
-            return el;
+        for(auto entry : counts) {
+            if(entry.second > nums.size() / 2) return entry.first;
         }
         return -1;
     }
 };
+
+
+
+// Moors Voting Algorithm
+// class Solution {
+// public:
+//     int majorityElement(vector<int>& nums) {
+//         int el;
+//         int count = 0;
+//         for(int i = 0; i < nums.size(); i++){
+//             if(count == 0) {
+//                 count = 1;
+//                 el = nums[i];
+//             }else if(nums[i] == el) {
+//                 count++;
+//             }else{
+//                 count--; 
+//             }
+//         }
+//         int count1 = 0;
+//         for(int i = 0; i < nums.size(); i++){
+//             if(nums[i] == el){
+//                 count1++;
+//             }
+//         }
+//         if(count1 > (nums.size()/2)) {
+//             return el;
+//         }
+//         return -1;
+//     }
+// };
