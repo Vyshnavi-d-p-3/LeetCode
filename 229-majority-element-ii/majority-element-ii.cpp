1bd0300086b8c79c60c2963a535dfe566176c44f
Vyshu <vyshnavi.dyvandinnepullareddy@gmail.com>
2026-04-24 18:38:16 -0700

    Time: 8 ms (31.10%) | Memory: 26.2 MB (36.58%) - LeetSync

--- 229-majority-element-ii/majority-element-ii.cpp
+++ 229-majority-element-ii/majority-element-ii.cpp
@@ -1,21 +1,40 @@
+// Approach: Hashing(TC- O(n), SC(O(n)))
+// class Solution {
+// public:
+//     vector<int> majorityElement(vector<int>& nums) {
+//         unordered_map<int, int> elementCount;
+//         int n = nums.size() / 3;
+//         vector<int> majorityElements;
+//         for(int num: nums) {
+//             if(elementCount.find(num) != elementCount.end()) {
+//                 elementCount[num]++;
+//             }else{
+//                 elementCount[num] = 1;
+//             }
+//         }
+//         for(auto element: elementCount) {
+//             if(element.second > n ) {
+//                 majorityElements.push_back(element.first);
+//             }
+//         }
+//         return majorityElements;
+//     }
+// };
+
 class Solution {
 public:
-    vector<int> majorityElement(vector<int>& nums) {
-        unordered_map<int, int> elementCount;
-        int n = nums.size() / 3;
-        vector<int> majorityElements;
-        for(int num: nums) {
-            if(elementCount.find(num) != elementCount.end()) {
-                elementCount[num]++;
-            }else{
-                elementCount[num] = 1;
+    vector<int> majorityElement(vector<int>& nums){
+        vector<int> res;
+        unordered_map<int,int> mpp;
+        int n = nums.size();
+        int min = (int)(n/3)+1;
+        for(int num: nums){
+            mpp[num]++;
+            if(mpp[num] == min) {
+                res.push_back(num);
             }
+            if(res.size() == 2) break;
         }
-        for(auto element: elementCount) {
-            if(element.second > n ) {
-                majorityElements.push_back(element.first);
-            }
-        }
-        return majorityElements;
+        return res;
     }
 };
