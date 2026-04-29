3dba33ff5b4489a2a00e8c6db6ed1714a1d622df
Vyshu <vyshnavi.dyvandinnepullareddy@gmail.com>
2026-04-28 17:05:56 -0700

    Time: 7 ms (56.16%) | Memory: 26.7 MB (75.88%) - LeetSync

--- 3227-find-missing-and-repeated-values/find-missing-and-repeated-values.cpp
+++ 3227-find-missing-and-repeated-values/find-missing-and-repeated-values.cpp
@@ -3,21 +3,22 @@ public:
     vector<int> findMissingAndRepeatedValues(vector<vector<int>>& grid) {
         int n = grid.size();
         int total = n*n;
-        vector<int> freq(total + 1, 0);
+        int repeating = -1, missing = -1;
+        vector<int> hash(total + 1, 0);
+
         for(int i = 0; i < n; i++){
             for(int j = 0; j < n; j++){
-                freq[grid[i][j]]++;
+                hash[grid[i][j]]++;
             }
         }
-        int repeated = -1;
-        int missing = -1;
-        for(int num = 1; num <= total; num++){
-            if(freq[num] == 2) {
-                repeated = num;
-            }else if(freq[num] == 0) {
-                missing = num;
-            }
+
+        for(int i = 1; i <= total; i++){
+            if(hash[i] == 2) repeating = i;
+            else if(hash[i] == 0) missing = i;
+            
+            if(repeating != -1 && missing != -1) break;
         }
-        return {repeated, missing};
+
+        return {repeating, missing};
     }
 };
