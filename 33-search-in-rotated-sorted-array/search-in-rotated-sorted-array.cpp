61c61d7b1bf46e01203a74ad39daa98b7c155495
Vyshu DP <vyshnavi.dyvandinnepullareddy@gmail.com>
2026-05-13 07:51:08 -0700

    Time: 0 ms (100.00%) | Memory: 15 MB (95.66%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
@@ -1,51 +1,23 @@
-class Solution {
-public:
-    int search(vector<int>& nums, int target) {
-        int low = 0, high = nums.size() - 1;
-
-        while(low <= high) {
-            int mid = (low + high) / 2;
-            if(nums[mid] == target) {
-                return mid;
-            }
-            if(nums[low] <= nums[mid]) {
-                if(nums[low] <= target && target < nums[mid]) {
-                    high = mid - 1;
-                } else {
-                    low = mid + 1;
-                }
-            } else {
-                if(nums[mid] < target && target <= nums[high]){
-                    low = mid + 1;
-                } else {
-                    high = mid - 1;
-                }
-            }
-        }
-        return -1;
-    }
-};
-
-
-// class Solution{
+// class Solution {
 // public:
-//     int search(vector<int> &nums, int target){
+//     int search(vector<int>& nums, int target) {
 //         int low = 0, high = nums.size() - 1;
+
 //         while(low <= high) {
 //             int mid = (low + high) / 2;
 //             if(nums[mid] == target) {
 //                 return mid;
 //             }
-//             if(nums[low] <= nums[mid]){
-//                 if(nums[low] < target && target <= nums[mid]) {
+//             if(nums[low] <= nums[mid]) {
+//                 if(nums[low] <= target && target < nums[mid]) {
 //                     high = mid - 1;
-//                 }else {
+//                 } else {
 //                     low = mid + 1;
 //                 }
-//             }else{
-//                 if(nums[mid] <= target && target < nums[high]) {
+//             } else {
+//                 if(nums[mid] < target && target <= nums[high]){
 //                     low = mid + 1;
-//                 }else{
+//                 } else {
 //                     high = mid - 1;
 //                 }
 //             }
@@ -53,3 +25,31 @@ public:
 //         return -1;
 //     }
 // };
+
+
+class Solution{
+public:
+    int search(vector<int> &nums, int target){
+        int low = 0, high = nums.size() - 1;
+        while(low <= high) {
+            int mid = (low + high) / 2;
+            if(nums[mid] == target) {
+                return mid;
+            }
+            if(nums[low] <= nums[mid]){
+                if(nums[low] <= target && target < nums[mid]) {
+                    high = mid - 1;
+                }else {
+                    low = mid + 1;
+                }
+            }else{
+                if(nums[mid] < target && target <= nums[high]) {
+                    low = mid + 1;
+                }else{
+                    high = mid - 1;
+                }
+            }
+        }
+        return -1;
+    }
+};
